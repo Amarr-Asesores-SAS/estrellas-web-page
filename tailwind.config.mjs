3324314b9aee8b964d1f126ecf76b5a9ce5a6c43
Juan Pablo Leon <juanleon8581@gmail.com>
2025-06-25 14:57:41 -0500

    refactor: migrate safelist to tailwind v4 sintax

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,31 +2,6 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}"],
-  safelist: [
-    {
-      pattern: /col-span-(\d+)/,
-      variants: ["lg"],
-    },
-    // Height
-    {
-      pattern: /h-(0|2|3|4|6|8|12|16|24|32)/,
-      variants: ["lg"],
-    },
-    // Text sizes for all screen sizes
-    {
-      pattern: /text-(xs|sm|base|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl|8xl|9xl)/,
-      variants: ["lg"],
-    },
-    // Font weights
-    {
-      pattern:
-        /font-(thin|extralight|light|normal|medium|semibold|bold|extrabold|black)/,
-    },
-    // Alignments
-    {
-      pattern: /text-(left|center|right)/,
-    },
-  ],
   theme: {
     extend: {
       fontFamily: {
