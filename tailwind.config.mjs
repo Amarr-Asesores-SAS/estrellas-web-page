1fe93869b86f9b71f46071987cd3ec3b42d512bc
Juan Pablo Leon <juanleon8581@gmail.com>
2025-06-25 13:04:14 -0500

    refactor: reorganize styles by removing tailwindConf and adding general.css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from "tailwindcss/defaultTheme";
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}"],
   safelist: [
     {
       pattern: /col-span-(\d+)/,
